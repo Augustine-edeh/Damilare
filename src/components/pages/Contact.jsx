42d4db6982f2edf5713b877261bc80e0e26937ff
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-02-22 04:16:54 +0100

    feat(Spinner): add loading spinner

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import emailjs from "@emailjs/browser";
+import { Spinner } from "flowbite-react";
 import { useState, useRef } from "react";
 import Card from "../ui/Card";
 import { ToastContainer, toast } from "react-toastify";
@@ -6,6 +7,26 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
   const [isSending, SetIsSending] = useState(false);
+
+  const IsSendingButtonUI = () => (
+    <button
+      type="button"
+      className="flex gap-x-3 bg-gray-800 text-white py-2 px-4 rounded-md opacity-80"
+    >
+      <Spinner size="md" className="animate-spin fill-blue-100" />
+      <span className="">Sending...</span>
+    </button>
+  );
+
+  const SendBottonUI = () => (
+    <button
+      type="submit"
+      id="submit-btn"
+      className="bg-gray-800 text-white py-2 px-4 rounded-md transition duration-300 transform hover:scale-105"
+    >
+      <span className="pl-3">Send Message</span>
+    </button>
+  );
 
   let [formInputsValues, setFormInputsValues] = useState({
     name: "",
@@ -113,14 +134,8 @@ const Contact = () => {
                 className="w-full border text-black border-gray-300 rounded-md p-3 focus:bg-indigo-50 focus:outline-none focus:border-blue-500 transition duration-300"
               ></textarea>
             </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                id="submit-btn"
-                className="bg-gray-800 text-white py-2 px-4 rounded-md transition duration-300 transform hover:scale-105"
-              >
-                {isSending ? "Sending..." : "Send Message"}
-              </button>
+            <div className="flex justify-center">
+              {isSending ? <IsSendingButtonUI /> : <SendBottonUI />}
             </div>
           </form>
         </Card>
